8846e82d26db973501335b38e51a1f9e23489f51
swolebrain <thecodingteacher@gmail.com>
2018-12-27 23:14:02 -0500

    fix more bugs with stupid kash allocation

--- src/services/PerformKiddieKashUpdates.js
+++ src/services/PerformKiddieKashUpdates.js
@@ -18,24 +18,29 @@ async function processAllChildAllowances() {
 
     const cursor = dbo.collection('familyunits').find().batchSize(10000);
     for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
-        if (!doc.kreditInformation){
-            bulkOp.find({_id: doc._id}).update({
-                $set: {
-                    'kreditInformation': {
-                        kiddieKashBalance: doc.allowanceAmount
+        doc.kidsList.forEach((kid, kidIndex) => {
+            if (!kid[kidIndex].kreditInformation){
+                const propName = `kidsList.${kidIndex}.kreditInformation`;
+                bulkOp.find({_id: doc._id}).update({
+                    $set: {
+                        [propName]: {
+                            kiddieKashBalance: kid.allowanceAmount
+                        }
                     }
-                }
-            });
-        }
-        else {
-            let currentBalance = doc.kreditInformation.kiddieKashBalance;
-            if (typeof currentBalance !== 'number') currentBalance = 0;
-            bulkOp.find({_id: doc._id}).update({
-                $set: {
-                    'kreditInformation.kiddieKashBalance': currentBalance + doc.allowanceAmount
-                }
-            });
-        }
+                });
+            }
+            else {
+                let currentBalance = doc.kreditInformation.kiddieKashBalance;
+                const propName = `kidsList.${kidIndex}.kreditInformation.kiddieKashBalance`;
+                if (typeof currentBalance !== 'number') currentBalance = 0;
+                bulkOp.find({_id: doc._id}).update({
+                    $set: {
+                        [propName]: currentBalance + kid.allowanceAmount
+                    }
+                });
+            }
+        });
+
     }
     bulkOp.execute(function(err, result){
         console.log(JSON.stringify(result, null, 4));
