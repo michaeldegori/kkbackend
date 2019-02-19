12b11f8eb72f73a2a20847d146ef9eadfe298cd7
swolebrain <thecodingteacher@gmail.com>
2019-02-19 17:35:06 -0500

    new dump and copy script

--- loadkkdb.sh
+++ loadkkdb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ~/backups
-mongoimport --db kiddiekredit --collection KidDashboardEI --out KidDashboardEI.json
-mongoimport --db kiddiekredit --collection ParentDashboardEI --out ParentDashboardEI.json
-mongoimport --db kiddiekredit --collection alerts --out alerts.json
-mongoimport --db kiddiekredit --collection builtinchores --out builtinchores.json
-mongoimport --db kiddiekredit --collection builtinrewards --out builtinrewards.json
-mongoimport --db kiddiekredit --collection choresuggestions --out choresuggestions.json
-mongoimport --db kiddiekredit --collection familyunits --out familyunits.json
-mongoimport --db kiddiekredit --collection users --out users.json
+mongoimport --db kiddiekredit --collection KidDashboardEI --file KidDashboardEI.json
+mongoimport --db kiddiekredit --collection ParentDashboardEI --file ParentDashboardEI.json
+mongoimport --db kiddiekredit --collection alerts --file alerts.json
+mongoimport --db kiddiekredit --collection builtinchores --file builtinchores.json
+mongoimport --db kiddiekredit --collection builtinrewards --file builtinrewards.json
+mongoimport --db kiddiekredit --collection choresuggestions --file choresuggestions.json
+mongoimport --db kiddiekredit --collection familyunits --file familyunits.json
+mongoimport --db kiddiekredit --collection users --file users.json
