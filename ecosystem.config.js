6ffca2b72f2614d5422e9de2ade914f3b7b4fe09
swolebrain <thecodingteacher@gmail.com>
2019-02-19 18:43:02 -0500

    changed some gitignore stuff and import script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,10 @@ module.exports = {
             },
             env_production : {
                 NODE_ENV: 'production',
+                PORT: 443
+            },
+            env_development : {
+                NODE_ENV: 'development',
                 PORT: 8080
             }
         },
